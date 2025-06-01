daddef0adadbbb7ba9561182ce567d81f8e0e487
ViMinhThang <huynhminhthang246@gmail.com>
2025-06-01 20:51:24 +0700

    fix issue #63

--- src/main/java/com/thomas/controller/CartRoutes/CheckoutReturn.java
+++ src/main/java/com/thomas/controller/CartRoutes/CheckoutReturn.java
@@ -16,6 +16,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet("/checkout-return")
@@ -24,18 +25,21 @@ public class CheckoutReturn extends HttpServlet {
     UploadOrderService uploadOrderService = new UploadOrderService();
     UploadPaymentMethod uploadPaymentMethod = new UploadPaymentMethod();
     UploadOrderDetailService uploadOrderDetailService = new UploadOrderDetailService();
+    CartService cartService = new CartService();
     EmailService emailService = new EmailService();
+    ProductService productService = new ProductService();
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
     }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = (User) request.getSession().getAttribute("auth");
         boolean isMoMo = request.getParameter("resultCode") != null; // MoMo có resultCode
         boolean isVNPay = request.getParameter("vnp_TransactionStatus") != null; // VNPay có vnp_TransactionStatus
 
         Map fields = new HashMap();
-        for (Enumeration params = request.getParameterNames(); params.hasMoreElements();) {
+        for (Enumeration params = request.getParameterNames(); params.hasMoreElements(); ) {
             String fieldName = URLEncoder.encode((String) params.nextElement(), StandardCharsets.US_ASCII.toString());
             String fieldValue = URLEncoder.encode(request.getParameter(fieldName), StandardCharsets.US_ASCII.toString());
             if ((fieldValue != null) && (fieldValue.length() > 0)) {
@@ -63,22 +67,20 @@ public class CheckoutReturn extends HttpServlet {
             request.setAttribute("total", total);
             request.setAttribute("status", status);
             request.setAttribute("message", "success");
-            createOrder(request, response);
-        }
-        else if (isVNPay && signValue.equals(vnp_SecureHash)) {
+            createOrder(request, response, user.getId(), user.getEmail());
+        } else if (isVNPay && signValue.equals(vnp_SecureHash)) {
             if ("00".equals(request.getParameter("vnp_TransactionStatus"))) {
                 request.setAttribute("paymentMethod", "VNPay");
                 request.setAttribute("orderId", request.getParameter("vnp_TxnRef"));
                 request.setAttribute("total", request.getParameter("vnp_Amount"));
                 request.setAttribute("status", status);
                 request.setAttribute("message", "success");
-                createOrder(request, response);
+                createOrder(request, response, user.getId(), user.getEmail());
             } else {
                 request.setAttribute("status", "Lỗi");
                 request.setAttribute("message", "failed");
             }
-        }
-        else {
+        } else {
             request.setAttribute("status", "Lỗi");
             request.setAttribute("message", "failed");
         }
@@ -86,34 +88,41 @@ public class CheckoutReturn extends HttpServlet {
         request.getRequestDispatcher("/frontend/cartPage/checkoutPage/checkout-return.jsp").forward(request, response);
     }
 
-    private void createOrder(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        HttpSession session = request.getSession();
-//        Map<String, CartItem> cart = (Map<String, CartItem>) session.getAttribute("cart");
-//        System.out.println("Cart: " + cart);
-//        Coupon cp = (Coupon) session.getAttribute("appliedCoupon");
-//        User user = (User) session.getAttribute("auth");
-//        int userId = user.getId();
-//        String paymentMethod = request.getParameter("resultCode") != null ? "MoMo" : "VNPay";
-//        Address address = uploadAddressService.getAddressByUserId(userId);
-//        int paymentMethodId = uploadPaymentMethod.getPaymentMethodId(paymentMethod);
-//        double totalPrice = 0, shippingCost = 0;
-//        for (CartItem cartItem : cart.values()) {
-//            totalPrice += cartItem.getPrice() * cartItem.getQuantity();
-//            shippingCost = cartItem.getQuantity() * 15.000;
-//        }
-//        double discountRate = cp == null ? 0 : cp.getDiscountRate();
-//        double discountAmount = totalPrice * (discountRate / 100);
-//        double grandTotal = totalPrice + shippingCost + discountAmount;
-//
-//        if (uploadOrderService.createOrder(userId, paymentMethodId, address.getId(), LocalDate.now(), grandTotal, "Đang xử lý", 0, user.getId())) {
-//            Order order = uploadOrderService.getLatestOrder();
-//            for (CartItem cartItem : cart.values()) {
-//                uploadOrderDetailService.createOrderDetail(order.getId(), cartItem.getPrice(), cartItem.getBelt().getId(), cartItem.getQuantity(), cartItem.getVariant().getId());
-//            }
-//        }
-//        String subject = "Thông báo đơn hàng";
-//        String content = "Đơn hàng của bạn đã được đặt thành công. Tổng giá trị đơn hàng là: " + totalPrice + " VNĐ.\n" +
-//                "Chi tiết đơn hàng:\n" + cart.toString() + "\n" + "cảm ơn bạn đã mua hàng tại cửa hàng của chúng tôi.\n";
-//        emailService.sendEmail(user.getEmail(), subject, content);
+    private void createOrder(HttpServletRequest request, HttpServletResponse response, int userId, String userEmail) throws IOException {
+        HttpSession session = request.getSession();
+        List<CartItem> cartItemList = cartService.getCart(userId);
+        Coupon cp = (Coupon) session.getAttribute("appliedCoupon");
+        String paymentMethod = request.getParameter("paymentMethod");
+        List<Address> address = uploadAddressService.getAddressList(userId);
+        Address userAddress = null;
+        for (Address addr : address) {
+            if (addr.getIsUse() == 1) {
+                userAddress = addr;
+            }
+        }
+        int paymentMethodId = uploadPaymentMethod.getPaymentMethodId(paymentMethod);
+        double totalPrice = 0, shippingCost = 0;
+        for (CartItem cartItem : cartItemList) {
+            totalPrice += cartItem.getPrice() * cartItem.getQuantity();
+            shippingCost = cartItem.getQuantity() * 15.000;
+        }
+        double discountRate = cp == null ? 0 : cp.getDiscountRate();
+        double discountAmount = totalPrice * (discountRate / 100);
+        double grandTotal = totalPrice + shippingCost + discountAmount;
+
+        if (uploadOrderService.createOrder(userId, paymentMethodId, userAddress.getId(), LocalDate.now(), grandTotal, "Đang xử lý", 0, userId)) {
+            Order order = uploadOrderService.getLatestOrder();
+            for (CartItem cartItem : cartItemList) {
+                for (CartItem item : cartItemList) {
+                    item.setBelt(productService.find(item.getBeltId()).get(0));
+                    item.setVariant(productService.findVariant(item.getBeltId(), item.getVariantId(), null, null));
+                }
+                uploadOrderDetailService.createOrderDetail(order.getId(), cartItem.getPrice(), cartItem.getBelt().getId(), cartItem.getQuantity(), cartItem.getVariant().getId());
+            }
+        }
+        String subject = "Thông báo đơn hàng";
+        String content = "Đơn hàng của bạn đã được đặt thành công. Tổng giá trị đơn hàng là: " + totalPrice + " VNĐ.\n" +
+                "Chi tiết đơn hàng:\n" + cartItemList.toString() + "\n" + "cảm ơn bạn đã mua hàng tại cửa hàng của chúng tôi.\n";
+        emailService.sendEmail(userEmail, subject, content);
     }
 }
